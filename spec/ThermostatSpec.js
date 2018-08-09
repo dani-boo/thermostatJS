b77982862172c845b90b6eb3c17db36aa3944721
dani <daniboo83@gmail.com>
2018-08-09 22:02:47 +0100

    First commit - setting up thermostat

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,22 @@
+'use strict'
+
+describe('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  // User story 1: thermostat starts at 20 degrees
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  // User story 2: increase temperature with up button
+  // it('increases temperature via up button', function() {
+  //   expect()
+  // })
+
+});
+
+
