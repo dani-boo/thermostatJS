ee8f3926cf1955d56fba46f56730696b629af440
dani <daniboo83@gmail.com>
2018-08-11 23:37:48 +0100

    Weather API included plus some refactoring

--- src/interface.js
+++ src/interface.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(Math.round(data.main.temp) + "°C");
+  })
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(Math.round(data.main.temp) + "°C")
+    })
+  })
+
   function changeTemperature() {
     $('#temperature').text(thermostat.temperature + "°C")
     $('#temperature').attr('class', thermostat.energyUsage());
   }
 
   $('#temperature').text(thermostat.temperature + "°C")
-
 
   $('#temp-up').on('click', function() {
     thermostat.up();
